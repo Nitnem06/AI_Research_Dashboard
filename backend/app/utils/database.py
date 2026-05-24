278101e7e0d427002b71d1f7a549073ced7245f6
Nitnem Juneja <junejanitnem1906@gmail.com>
2026-05-24 22:19:04 +0530

    moving wihtout async engine

--- backend/app/utils/database.py
+++ backend/app/utils/database.py
@@ -1,8 +1,8 @@
-from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession, async_sessionmaker
+from sqlalchemy.ext.asyncio import create_engine, AsyncSession, async_sessionmaker
 from sqlalchemy.orm import DeclarativeBase
 from app.core.config import settings
 
-engine = create_async_engine(
+engine = create_engine(
     settings.DATABASE_URL,
     echo=settings.DEBUG,
     pool_pre_ping=True,
