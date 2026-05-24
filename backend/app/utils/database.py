957006a926dae35590fe1135c79a5086d9e464de
Nitnem Juneja <junejanitnem1906@gmail.com>
2026-05-24 22:30:00 +0530

    moving wihtout async engine

--- backend/app/utils/database.py
+++ backend/app/utils/database.py
@@ -1,4 +1,5 @@
-from sqlalchemy.ext.asyncio import create_engine, AsyncSession, async_sessionmaker
+from sqlalchemy.ext.asyncio import  AsyncSession, async_sessionmaker
+from sqlalchemy import create_engine
 from sqlalchemy.orm import DeclarativeBase
 from app.core.config import settings
 
