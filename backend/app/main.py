db2c5103e55b4a83a7bb2d92e53603bb28a30714
Nitnem06 <junejanitnem1906@gmail.com>
2026-05-26 01:21:06 +0530

    adding deployed frontend URL

--- backend/app/main.py
+++ backend/app/main.py
@@ -50,7 +50,7 @@ app = FastAPI(
 # CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[settings.FRONTEND_URL, "http://localhost:3000"],
+    allow_origins=[settings.FRONTEND_URL, "https://ai-research-dashboard-kkbixpm5i-nitnem-s-projects.vercel.app/login"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
