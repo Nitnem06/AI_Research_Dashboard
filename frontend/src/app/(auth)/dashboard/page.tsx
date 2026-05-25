81e934dc051278b9df28a76176692e4b21522b39
Nitnem Juneja <junejanitnem1906@gmail.com>
2026-05-25 20:03:32 +0530

    committing frontend folder

--- frontend/src/app/(auth)/dashboard/page.tsx
+++ frontend/src/app/(auth)/dashboard/page.tsx
@@ -27,7 +27,7 @@ export default function DashboardPage() {
     try {
       setLoading(true)
 
-      const res = await fetch("http://127.0.0.1:8000/research/search", {
+      const res = await fetch("${process.env.NEXT_PUBLIC_API_URL}/search", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
